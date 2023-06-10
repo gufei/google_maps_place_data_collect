eaebd59b6b75ea98c659593e6f4737e04248e3a2
rowan.wu <>
2023-06-10 20:32:44 +0800

    增加按类别采集

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ if not googleKey:
 TIMEOUT = 30
 
 keywordArr = os.getenv('KEYWORDS', '').split(',')
+typeArr = os.getenv('SEARCH_TYPE').split(',')
+
+searchMode = os.getenv('SEARCH_MODE', 'keyword')
 
 count = dict()
 countLine = dict()
@@ -35,43 +38,49 @@ outfile = dict()
 pbar = dict()
 
 # 参数，该参数通过 地理编码服务获取 https://maps.googleapis.com/maps/api/geocode/json?language=zh-CN&address=%E6%96%B0%E5%8A%A0%E5%9D%A1&key={googleKey}
-lonRange = [os.getenv('COLLECT_BOUNDS_WEST', 103.594),
-            os.getenv('COLLECT_BOUNDS_EAST', 104.0945001)]  # the range of longitude 经度的范围
-latRange = [os.getenv('COLLECT_BOUNDS_SOUTH', 1.1496),
-            os.getenv('COLLECT_BOUNDS_NORTH', 1.4784001)]  # the range of latitude 纬度的范围
-lonDivision = os.getenv('LNG_DIVISION', 0.005)  # 分块查询，每格约0.4km
-latDivision = os.getenv('LAT_DIVISION', 0.005)  # 分块查询，每格约0.4km
-radius = os.getenv('RADIUS', 500)  # 查询参数 半径 500m
+lonRange = [float(os.getenv('COLLECT_BOUNDS_WEST', 103.594)),
+            float(os.getenv('COLLECT_BOUNDS_EAST', 104.0945001))]  # the range of longitude 经度的范围
+latRange = [float(os.getenv('COLLECT_BOUNDS_SOUTH', 1.1496)),
+            float(os.getenv('COLLECT_BOUNDS_NORTH', 1.4784001))]  # the range of latitude 纬度的范围
+lonDivision = float(os.getenv('LNG_DIVISION', 0.005))  # 分块查询，每格约0.4km
+latDivision = float(os.getenv('LAT_DIVISION', 0.005))  # 分块查询，每格约0.4km
+radius = int(os.getenv('RADIUS', 500))  # 查询参数 半径 500m
 
 dirpath = os.getenv('DATA_DIR', 'data')
 if not os.path.exists(dirpath):
     os.makedirs(dirpath)
 
 
-def xjp_cj(keyword):
+def xjp_cj(keyword=None, search_type=None):
     global count
-    # keyword = "beauty"
-    outfile[keyword] = dirpath + "/" + keyword + ".csv"
-    count[keyword] = 0
-    countLine[keyword] = 0
 
-    if os.path.isfile(outfile[keyword]):
-        df[keyword] = pd.read_csv(outfile[keyword], index_col="place_id")
+    if searchMode == "keyword":
+        key = keyword
+    elif searchMode == "type":
+        key = search_type
+
+    count[key] = 0
+    countLine[key] = 0
+
+    outfile[key] = dirpath + "/" + key + ".csv"
+
+    if os.path.isfile(outfile[key]):
+        df[key] = pd.read_csv(outfile[key], index_col="place_id")
     else:
-        df[keyword] = pd.DataFrame(
+        df[key] = pd.DataFrame(
             columns=['place_id', '商户名称', '商户评分', '评价数', '商户品类', '商户地址', '营业时间', '运营状态',
                      '官网地址',
                      '电话', '坐标', '最新评论时间'])
-        df[keyword].set_index('place_id', inplace=True)
-        df[keyword].to_csv(outfile[keyword], index=True)
+        df[key].set_index('place_id', inplace=True)
+        df[key].to_csv(outfile[key], index=True)
 
     lonArr = numpy.arange(lonRange[0], lonRange[1], lonDivision)
     latArr = numpy.arange(latRange[0], latRange[1], latDivision)
     tqdmNum = len(lonArr) * len(latArr)
-    print(keyword + ' 开始爬取')
-    print(keyword + ' 共有' + str(tqdmNum) + '次请求')
+    print(key + ' 开始爬取')
+    print(key + ' 共有' + str(tqdmNum) + '次请求')
 
-    pbar[keyword] = tqdm(total=tqdmNum)
+    pbar[key] = tqdm(total=tqdmNum)
 
     for lon in lonArr:
         # print('已进行' + str(count) + '次请求，得到' + str(countLine) + '条有效信息')
@@ -79,13 +88,13 @@ def xjp_cj(keyword):
             # print('已进行' + str(count) + '次请求，得到' + str(countLine) + '条有效信息')
             #   发请求
             latlon = str(lat) + ',' + str(lon)
-            get_search(latlon, radius, keyword)
-            count[keyword] = count[keyword] + 1
+            get_search(latlon, radius, key=key)
+            count[key] = count[key] + 1
     multitasking.wait_for_tasks()
 
-    print(keyword + ' 已进行' + str(count[keyword]) + '次请求，得到' + str(countLine[keyword]) + '条有效信息')
+    print(key + ' 已进行' + str(count[key]) + '次请求，得到' + str(countLine[key]) + '条有效信息')
 
-    print(keyword + ' 结束')
+    print(key + ' 结束')
 
 
 def json_request(url: string):
@@ -95,14 +104,14 @@ def json_request(url: string):
     return json.loads(response)
 
 
-def get_data(keyword, place_id=None, item=None):
+def get_data(key, place_id=None, item=None):
     global countLine
     if not place_id:
         place_id = item['place_id']
         types = item['types']
 
-    if place_id in df[keyword].index.values:
-        countLine[keyword] = countLine[keyword] + 1
+    if place_id in df[key].index.values:
+        countLine[key] = countLine[key] + 1
         return
 
     basic_url = 'https://maps.googleapis.com/maps/api/place/details/json?key={0}&language=zh-CN&place_id={1}&reviews_sort=newest'
@@ -130,20 +139,26 @@ def get_data(keyword, place_id=None, item=None):
 
     }
     # 多线程，再判断一次place_id是否存在
-    if place_id in df[keyword].index.values:
-        countLine[keyword] = countLine[keyword] + 1
+    if place_id in df[key].index.values:
+        countLine[key] = countLine[key] + 1
         return
-    df[keyword].loc[place_id] = data
+    df[key].loc[place_id] = data
     data_df = pd.DataFrame([data])
-    data_df.to_csv(outfile[keyword], mode='a', index=False, header=False)
+    data_df.to_csv(outfile[key], mode='a', index=False, header=False)
 
-    countLine[keyword] = countLine[keyword] + 1
+    countLine[key] = countLine[key] + 1
 
 
 @multitasking.task
-def get_search(location, radius, keyword, next_page_token=None):
-    basic_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={0}&location={1}&radius={2}&keyword={3}&language=zh-CN'
-    url = basic_url.format(googleKey, location, radius, keyword)
+def get_search(location, radius, key, next_page_token=None):
+    if searchMode == "keyword":
+        basic_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={0}&location={1}&radius={2}&keyword={3}&language=zh-CN'
+    elif searchMode == "type":
+        basic_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={0}&location={1}&radius={2}&type={3}&language=zh-CN'
+
+    url = basic_url.format(googleKey, location, radius, key)
+
+
 
     if next_page_token:
         url = url + "&pagetoken=" + next_page_token
@@ -154,8 +169,9 @@ def get_search(location, radius, keyword, next_page_token=None):
     responseJSON = json_request(url)
     if responseJSON['status'] != "OK":
         if not next_page_token:
-            pbar[keyword].update()
+            pbar[key].update()
         return
+    print(url)
     for item in responseJSON['results']:
         if 'plus_code' in item \
                 and (
@@ -163,15 +179,20 @@ def get_search(location, radius, keyword, next_page_token=None):
                 and "新加坡" not in item['plus_code']['compound_code'] \
                 and "Singapore" not in item['plus_code']['compound_code']:
             continue
-        get_data(keyword=keyword, item=item)
+        get_data(key=key, item=item)
 
     if "next_page_token" in responseJSON:
-        get_search(location, radius, keyword, next_page_token)
+        get_search(location, radius, key, next_page_token)
     else:
-        pbar[keyword].update()
+        pbar[key].update()
 
 
 if __name__ == "__main__":
-    for keyword in keywordArr:
-        if keyword.strip():
-            xjp_cj(keyword.strip())
+    if searchMode == "keyword":
+        for keyword in keywordArr:
+            if keyword.strip():
+                xjp_cj(keyword=keyword.strip())
+    elif searchMode == "type":
+        for search_type in typeArr:
+            if search_type.strip():
+                xjp_cj(search_type=search_type.strip())
